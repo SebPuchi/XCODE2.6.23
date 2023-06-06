1fd32823d9a5e937d3432b06a87e87eb6061e424
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2023-06-06 09:12:59 -0400

    Initial Commit

---
+++ tues6.6.23/tues6_6_23App.swift
@@ -0,0 +1,17 @@
+//
+//  tues6_6_23App.swift
+//  tues6.6.23
+//
+//  Created by Sebastian Pucher on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct tues6_6_23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tues6.6.23UITests/tues6_6_23UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tues6_6_23UITestsLaunchTests.swift
+//  tues6.6.23UITests
+//
+//  Created by Sebastian Pucher on 6/6/23.
+//
+
+import XCTest
+
+final class tues6_6_23UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
